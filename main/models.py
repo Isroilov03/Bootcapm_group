d05c3fbd91a944dec167dcd31ba5de01ed9f3ec4
ibrohimjon1209090 <ibrohimjonmamirov12092008@gmail.com>
2023-09-09 16:56:41 +0500

    meniki

--- main/models.py
+++ main/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
+
+
+
+class Notbuk(models.Model):
+    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
